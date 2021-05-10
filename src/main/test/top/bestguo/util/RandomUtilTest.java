e42d9a72874fa7fe73bc56eb9e64b6f2ac3fc2a3
bestguo2020 <bestguo2020@foxmail.com>
2021-05-10 16:53:19 +0800

    随机数工具类测试

---
+++ src/main/test/top/bestguo/util/RandomUtilTest.java
@@ -0,0 +1,12 @@
+package top.bestguo.util;
+
+import org.junit.Test;
+
+public class RandomUtilTest {
+
+    @Test
+    public void test1() {
+        System.out.println(RandomUtils.getRandomInt(10, 100));
+    }
+
+}
