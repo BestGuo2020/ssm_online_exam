34cf3605f3cd4027cd705289ff1ba41c2521a057
bestguo2020 <bestguo2020@foxmail.com>
2021-05-11 13:05:49 +0800

    设置setter和getter方法

--- src/main/java/top/bestguo/render/DataResult.java
+++ src/main/java/top/bestguo/render/DataResult.java
@@ -3,7 +3,8 @@ package top.bestguo.render;
 import java.util.List;
 
 /**
- * 返回数据类，一般运用于加载表格的数据
+ * 返回数据类，一般运用于加载表格的数据。
+ * 该类适用于分页。
  *
  * @param <T> 传入哪种是有蹄类
  */
@@ -15,12 +16,12 @@ public class DataResult<T> {
     private Integer code;
 
     /**
-     * 返回的消息
+     * 返回的消息提示信息
      */
     private String message;
 
     /**
-     * 返回数据的个数
+     * 返回的数据总数（不是每一页的数据个数）
      */
     private Integer total;
 
@@ -29,5 +30,35 @@ public class DataResult<T> {
      */
     private List<T> data;
 
+    public Integer getCode() {
+        return code;
+    }
 
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
 }
