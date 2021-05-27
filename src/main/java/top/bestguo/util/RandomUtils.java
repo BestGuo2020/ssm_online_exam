ff8320e1dc6e5c00b177fc7048445c6b38f58ca7
bestguo2020 <bestguo2020@foxmail.com>
2021-05-27 23:39:07 +0800

    优化了随机数生成办法，解决了随机生数个数小于整数随机数范围导致的死循环

--- src/main/java/top/bestguo/util/RandomUtils.java
+++ src/main/java/top/bestguo/util/RandomUtils.java
@@ -14,12 +14,16 @@ public class RandomUtils {
     /**
      * 生成整数随机数，不重复
      *
-     * @param count 生成整数随机数
+     * @param count 生成整数随机数个数
      * @param size 生成整数随机数范围
      * @return
      */
     public static LinkedHashSet<Integer> getRandomInt(int count, int size) {
         LinkedHashSet<Integer> set = new LinkedHashSet<Integer>();
+        if(count > size) {
+            throw new IllegalArgumentException("生成整数的随机个数不得大于生成整数随机" +
+                    "数范围：count:" + count + " > " + "size:" + size);
+        }
         while (set.size() < count) {
             Integer tmp = random.nextInt(size);
             set.add(tmp);
